3134b4a2194c984f07b56c7e9d4c7e12237bb41b
Grace Lee <glee3890@gmail.com>
2023-10-17 07:27:57 -0400

    Added Print Statement

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -2,3 +2,7 @@
 # Date: 10.11.2023
 # Program: Sandbox Playground
 
+print("Hello Grace Lee!")
+print("\nI am looking forward to the weekend!")
+print("I am going to be doing a band compitition this Saturday. ")
+print("This is Monday.")
